570c2fe2aa07a4ce7e04c0e32b2253db9c85f247
Jagadeesh Kadavakollu <k.jagadeesh123@gmail.com>
2023-07-20 16:28:19 +0530

    Conditinal rendering, http methods examples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React, {useState} from 'react';
 import './App.css';
 import Counter from './counter';
 import DogsList from './dogs-list';
 
 function App() {
-  console.log('parent rendered')
+  const [showPage, setPage] = useState('dogs-list');
+
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <DogsList />
+      <div>
+      <hr />
+      <button onClick={() => setPage('counter')}>Show counter</button>
+      <button onClick={() => setPage('dogs-list')}>Dogs List</button>
+      <hr />
+      {showPage === "counter" &&
+        <Counter />
+      }
+      {showPage === "dogs-list" &&
+        <DogsList />
+      }
+      </div>
     </div>
   );
 }
--- src/dogs-list.js
+++ src/dogs-list.js
@@ -2,22 +2,45 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 function DogsList () {
-    const [dogData, setDogsData] = useState([])
+    const [dogsData, setDogsData] = useState([])
+    console.log(dogsData, 'dogs data ....')
 
     useEffect(() => {
         function fetchData () {
             axios.get("https://api.thedogapi.com/v1/breeds/search?name=dog")
-            // .then(({ data }) => {
-            //     console.log(data, 'data')
-            // })
-            // .catch((error) => console.log(error));
-            // }
+                .then(({ data }) => {
+                    setDogsData(data);
+                })
+                .catch((error) => console.log(error));
         }
         fetchData();
     }, []);
 
     return (
-        <div>Dogs list</div>
+        <div>
+            <div>Dogs list</div>
+            <table>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Life Span</th>
+                <th>Breed Group</th>
+                <th></th>
+                <th></th>
+              </tr>
+              {dogsData.map(({id, name, life_span, breed_group}) => 
+                <tr>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{life_span}</td>
+                  <td>{breed_group}</td>
+                  <td><button>Update</button></td>
+                  <td><button>Delete</button></td>
+                </tr>
+              )}
+            </table>
+        </div>
+
     )
 }
 
